3695504236d19905258c51c09d36f5ca6fe20e17
Asadulnyc <asadulk@nycstudents.net>
2025-05-16 15:28:14 +0000

    serwe

---
+++ DrawingExample.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawingExample extends JFrame {
+
+    public DrawingExample() {
+        setTitle("Drawing Example");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(new DrawingPanel());
+        setVisible(true);
+    }
+
+    class DrawingPanel extends JPanel {
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.BLUE);
+            g.fillRect(50, 50, 100, 80);
+            g.setColor(Color.RED);
+            g.drawString("Hello, Java Graphics!", 100, 200);
+        }
+    }
+}
